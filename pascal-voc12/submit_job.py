a5a56cdfeb8fd3c83f8fd2a2b0948c75b70704fa
vfdev-5 <vfdev.5@gmail.com>
2021-05-05 00:02:35 +0200

    Added confs with AdamP and SGDP from https://github.com/clovaai/AdamP

--- pascal-voc12/submit_job.py
+++ pascal-voc12/submit_job.py
@@ -8,6 +8,7 @@ assert "CLEARML_API_SECRET_KEY" in os.environ
 
 n_gpus = 4
 job_cmd = f"""pip install fire py-config-runner git+https://github.com/vfdev-5/ImageDatasetViz.git albumentations opencv-python-headless clearml \
+&& pip install adamp \
 && export CLEARML_API_HOST="https://api.community.clear.ml" \
 && export CLEARML_WEB_HOST="https://app.community.clear.ml" \
 && export CLEARML_FILES_HOST="https://files.community.clear.ml" \
@@ -15,7 +16,7 @@ job_cmd = f"""pip install fire py-config-runner git+https://github.com/vfdev-5/I
 && export SBD_DATASET_PATH=$TRAINML_DATA_PATH/VOCdevkit/VOCaug/dataset/ \
 && cd pascal-voc12 \
 && nvidia-smi \
-&& export config_file=configs/baseline_dplv3_resnet101_sbd.py \
+&& export config_file=configs/adamp_dplv3_resnet101_sbd.py \
 && python -u -m torch.distributed.launch --nproc_per_node={n_gpus} --use_env main.py training $config_file
 """
 
